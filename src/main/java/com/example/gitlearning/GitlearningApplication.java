b10d587c2ee70e6953f16e56fcc5fe5c7ad4b4c7
Gaurav <ankurraj2209@gmail.com>
2026-09-03 21:44:53 +0530

    1st commit by repo owner

---
+++ src/main/java/com/example/gitlearning/GitlearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlearningApplication.class, args);
+    }
+
+}
